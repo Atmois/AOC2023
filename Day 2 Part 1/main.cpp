68604fde4de74f6a19f250b71c55b397161beaf8
Atmois <atmois@outlook.com>
2023-12-02 13:59:37 +0000

    day 2 attempt

--- Day 2 Part 1/main.cpp
+++ Day 2 Part 1/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 
-int findGameN(std::string string)
+int findGameNum(std::string string)
 {
     std::string num1S;
     std::string num2S;
@@ -49,14 +49,99 @@ int findGameN(std::string string)
 
 int main()
 {
+    const int redLimit = 12;
+    const int greenLimit = 13;
+    const int blueLimit = 14;
+    int redCubes;
+    int greenCubes;
+    int blueCubes;
+
     int gameNum;
+    int colonPos;
+    int semicolonPos;
+    bool checkRun = true;
     std::string string = "Game 123: 12 red, 2 green, 5 blue; 9 red, 6 green, 4 blue; 10 red, 2 green, 5 blue; 8 blue, 9 red";
-    std::string string2 = "needle";
+    /*
+     */
+    gameNum = findGameNum(string);
 
-    gameNum = findGameN(string);
-
-    if (string.find(string2) != std::string::npos)
+    colonPos = string.find(":");
+    colonPos = colonPos + 1;
+    if (colonPos != std::string::npos)
     {
-        std::cout << "!";
+        string = string.substr(colonPos + 1);
+    }
+
+    semicolonPos = string.find(";");
+    std::string cubeSubsets[4];
+    int i = 0;
+    while (checkRun == true && i < 3)
+    {
+        if (semicolonPos != std::string::npos)
+        {
+            cubeSubsets[i] = string.substr(0, semicolonPos);
+            semicolonPos = semicolonPos + 1;
+            string = string.substr(semicolonPos + 1);
+            semicolonPos = string.find(";");
+        }
+        else
+        {
+            checkRun = false;
+        }
+        i++;
+    }
+
+    /*
+    std::cout << cubeSubsets[0] << "\n"
+              << cubeSubsets[1] << "\n"
+              << cubeSubsets[2] << "\n"
+              << cubeSubsets[3] << "\n";
+    */
+
+    checkRun = true;
+    std::string totalNumS;
+    std::string totalNum1;
+    std::string totalNum2;
+    int totalNum;
+
+    std::string colour;
+    int colourPos;
+    while (checkRun == true)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < cubeSubsets[i].length(); j++)
+            {
+                if (isdigit(cubeSubsets[i][j]))
+                {
+                    totalNum1 = cubeSubsets[i][j];
+                    if (j + 1 < cubeSubsets[i].length() && isdigit(cubeSubsets[i][j + 1]))
+                    {
+                        totalNum2 = cubeSubsets[i][j + 1];
+                        totalNumS = totalNum1 + totalNum2;
+                        totalNum = std::stoi(totalNumS);
+                        checkRun = false;
+                    }
+                    
+                    std::string leftovers = cubeSubsets[i].substr(j + 1);
+                    if ((colourPos = leftovers.find("red")) != std::string::npos)
+                    {
+                        colour = "red";
+                    }
+                    else if ((colourPos = leftovers.find("green")) != std::string::npos)
+                    {
+                        colour = "green";
+                    }
+                    else if ((colourPos = leftovers.find("blue")) != std::string::npos)
+                    {
+                        colour = "blue";
+                    }
+                }
+                std::cout << totalNum << " " << colour << "\n";
+            }
+        }
+        checkRun = false;
     }
 }
+
+// Take string find first num then remove that bit and check if that num is under limit if yes continue then do the same until end, if all are valid then add game num to total num
