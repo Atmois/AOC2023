75c60d7818fc075f6c7b69a0e59cdcb658817853
Atmois <130537361+Atmois@users.noreply.github.com>
2023-12-02 00:11:52 +0000

    day 1 part 1

---
+++ Day 1 Part 1/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+int main()
+{
+    std::ifstream inputF("input.txt");
+    std::string textS;
+    bool checkRun = true;
+    char checkingLetter;
+    std::string num1;
+    std::string num2;
+    std::string totalS;
+    double total;
+    double overall = 0;
+
+    while (std::getline(inputF, textS))
+    {
+        while (checkRun == true)
+        {
+            for (int i = 0; i < textS.length(); i++)
+            {
+                checkingLetter = textS[i];
+                if (isdigit(checkingLetter))
+                {
+                    num1 = std::string(1, checkingLetter);
+                    checkRun = false;
+                    break;
+                }
+            }
+        }
+        reverse(textS.begin(), textS.end());
+        checkRun = true;
+        while (checkRun == true)
+        {
+            for (int i = 0; i < textS.length(); i++)
+            {
+                checkingLetter = textS[i];
+                if (isdigit(checkingLetter))
+                {
+                    num2 = std::string(1, checkingLetter);
+                    checkRun = false;
+                    break;
+                }
+            }
+        }
+        checkRun = true;
+        totalS = num1 + num2;
+        total = std::stod(totalS);
+        std::cout << total << std::endl;
+        overall = overall + total;
+    }
+    std::cout << overall;
+    inputF.close();
+}
